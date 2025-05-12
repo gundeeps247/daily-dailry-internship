927831f70b174443a806c26d0a9d7182ea0ec3b4
2115046 <gundeepsinghm@gmail.com>
2025-05-12 19:21:39 +0530

    initial commit

---
+++ task/task1/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Welcome to MERN Stack Training</h1>
+      <p>Let's build amazing applications with React!</p>
+    </div>
+  );
+};
+
+export default App;
